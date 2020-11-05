288360ebcd5638fcdd589f589e9d2a5bcc7e77dd
Paulo Viana <paulo9mv@yahoo.com.br>
2020-11-05 16:43:27 -0400

    Adiciona fetch à API de moedas

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { fetchApi } from "../store/reducer";
 
 export function Content() {
+  const dispatch = useDispatch();
+
+  dispatch(fetchApi())
+
   return (
     <div>
       Hello World
---
+++ src/store/reducer.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { AppThunk, RootState } from "./store";
+
+const initialState = {
+
+};
+
+//Update state
+export const apiSlice = createSlice({
+  name: "api",
+  initialState,
+  reducers: {
+    
+  },
+});
+
+//Async functions
+export const fetchApi = (): AppThunk => (dispatch) => {
+  fetch("https://economia.awesomeapi.com.br/all/USD-BRL")
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch(function (error) {
+      console.log(error.message);
+    });
+};
+
+export default apiSlice.reducer;
