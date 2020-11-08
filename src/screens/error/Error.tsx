de11f75ef3f25d1533d0b3f385ced7d48d4e93ff
Paulo Viana <paulo9mv@yahoo.com.br>
2020-11-08 17:24:07 -0400

    Renomeia pasta assets/img -> assets/svg

--- src/screens/error/Error.tsx
+++ src/screens/error/Error.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 
-import image from '../../assets/img/robot_error.svg'; // Tell webpack this JS file uses this image
+import errorImage from '../../assets/svg/robot_error.svg'; // Tell webpack this JS file uses this image
 
 import 'antd/dist/antd.css';
 import './Error.css'
@@ -11,7 +11,7 @@ import './Error.css'
 export const Error: FunctionComponent = () => {
   return (
     <div className='error'>
-      <img src={image} alt="Logo"/>
+      <img src={errorImage} alt="Logo"/>
       Ocorreu um erro. Tente novamente mais tarde.
     </div>
   );
